8cac377f195e3c0865fd3e546dd7bcee4cca2946
olena <denaolena@gmail.com>
2023-05-02 15:00:42 +0300

    виконує дз 11

---
+++ src/fetchPictures.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+// export async function fetchPhoto(inputValue, page) {
+//   const queryParams = {
+//     key: '35912782-f9206aec1cfed4286d9fa7302',
+//     q: inputValue,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     page: page,
+//     per_page: 40,
+//   };
+//   try {
+//     const response = await axios.get('https://pixabay.com/api/', {
+//       params: queryParams,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+async function fetchPictures(query, page, per_page) {
+  const params = new URLSearchParams({
+    key: '35912782-f9206aec1cfed4286d9fa7302',
+    q: query,
+    page: page,
+    per_page: per_page,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  try {
+    const response = await axios.get(`https://pixabay.com/api/?${params}`);
+
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default fetchPictures;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,103 @@
 import './css/styles.css';
-import { fetchPhoto } from './fetchPhoto';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+import fetchPictures from './fetchPictures';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
-const searchForm = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const inputForm = document.querySelector('input');
+let page = 1;
+let per_page = 40;
+
+const form = document.querySelector('.search-form');
+const inputEl = document.querySelector('.search-form_input');
+const galleryEl = document.querySelector('.gallery');
+const button = document.querySelector('.load-more');
+let query = null;
+
+function renderGallery(images) {
+  const galleryList = images
+    .map(images => {
+      return `<div class="photo-card">
+  <a class="photo-card__link" href="${images.largeImageURL}" rel="noopener noreferrer">
+  <img class="photo-card__img" src="${images.webformatURL}" alt="${images.tags}" loading="lazy"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${images.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${images.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${images.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${images.downloads}
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', galleryList);
+  simpleLightbox.refresh();
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery a ', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+const handleSubmit = async e => {
+  e.preventDefault();
+  page = 1;
+  button.style.display = 'none';
+  galleryEl.innerHTML = '';
+  const elements = form.elements;
+  query = elements.searchQuery.value;
+  const images = await fetchPictures(query, page, per_page);
+
+  if (images.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+
+    // return false;
+  }
+
+  renderGallery(images.hits);
+  button.style.display = 'block';
+  page = page + 1;
+};
+form.addEventListener('submit', e => handleSubmit(e));
+
+const handleClick = async e => {
+  button.style.display = 'none';
+  const images = await fetchPictures(query, page, per_page);
+  renderImages(images.hits);
+  button.style.display = 'block';
+
+  page = page + 1;
+
+  if ((page - 1) * per_page >= images.totalHits) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    button.style.display = 'none';
+  }
+};
+
+button.addEventListener('click', handleClick);
+
+// function smoothScroll() {
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
